aa14abc2ae68aa0a0eceaf1c999f88297550dbd0
Gianmaria Saggini <gianmaria.saggini@gmail.com>
2023-01-30 14:29:58 +0100

    authentication server side.

--- global/javascript/form-validation.js
+++ global/javascript/form-validation.js
@@ -149,7 +149,7 @@ const isNameSurnameValid = nameSurname => {
 };
 
 const isEmailValid = email => {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const re = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
     return re.test(email);
 };
 
